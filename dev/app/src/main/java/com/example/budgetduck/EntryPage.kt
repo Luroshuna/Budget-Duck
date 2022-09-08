da5f0a58683f4859a247344a99c25d7016121c32
Luroshuna <luroshuna@gmail.com>
2022-09-08 15:36:55 +0200

    icon, working version but without real Entry Array

--- dev/app/src/main/java/com/example/budgetduck/EntryPage.kt
+++ dev/app/src/main/java/com/example/budgetduck/EntryPage.kt
@@ -8,6 +8,7 @@ import android.text.TextUtils
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
+import java.io.Serializable
 
 
 class EntryPage : AppCompatActivity() {
@@ -19,8 +20,10 @@ class EntryPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page_entry)
 
-        fortune = intent.getStringExtra("amount") ?: ""
+        //Get the amount of your fortune, this is the big number at the top of the screen on the main activity
+        fortune = intent.getStringExtra("amount") ?: "0"
 
+        //Get all the fields from the create entry activity
         val amount = findViewById<EditText>(R.id.editTextNumberDecimal)
         val category = findViewById<RadioGroup>(R.id.radio_group)
         val isExpense = findViewById<SwitchCompat>(R.id.switch1)
@@ -47,41 +50,42 @@ class EntryPage : AppCompatActivity() {
             var isValid = true
             val entry = Entry("", "", false)
 
+            //Check if an amount is entered
             if (TextUtils.isEmpty(amount.text)) {
                 showError("No amount entered!")
                 isValid = false
             }
 
+            //Check if a category is selected
             val selectedRadioButtonId: Int = category.checkedRadioButtonId
-
             if (selectedRadioButtonId == -1) {
                 showError("No category selected!")
                 isValid = false
             }
 
+            //If both of the above is valid -> do the math and set the values
             if (isValid) {
+                //Get all the values and push them into the array (sadly I couldn't get the pushed data to the other activity)
                 entry.amount = amount.text.toString()
                 val checkedRadioButton: RadioButton = findViewById(selectedRadioButtonId)
                 entry.category = checkedRadioButton.text.toString()
                 entry.isExpense = isExpense.isChecked
                 entries.addEntry(entry)
 
+                //Calculate the new fortune -> old fortune we get at the top of this onCreated() + the new entered amount
                 calculateNewFortune()
 
-                Toast.makeText(
-                    this,
-                    "Entry successfully created! $fortune",
-                    Toast.LENGTH_LONG
-                ).show()
-
+                //Send the new calculated fortune to the other activity
                 val intent = Intent(this, MainActivity::class.java)
                 intent.putExtra("amount", fortune)
+                /*intent.putExtra("entries", entries)*/
                 startActivity(intent)
             }
         }
     }
 
     private fun showError(message: String) {
+        //If you would've entered something incorrect, you would get an error alert dialog
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
         builder.setMessage(message)
@@ -91,10 +95,12 @@ class EntryPage : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     private fun calculateNewFortune() {
+        //First get the last entry of the array -> this would be the amount you just entered
         var entry = 0.0
         val lastEntry = entries.getEntries().last()
         var lastFortune = 0.0
 
+        //Try to parse the new entry and the old fortune
         try {
             lastFortune = fortune.toDouble()
             entry = lastEntry.amount.toDouble()
@@ -102,10 +108,12 @@ class EntryPage : AppCompatActivity() {
             Toast.makeText(this, "There was an Error: $e", Toast.LENGTH_LONG).show()
         }
 
+        //If you've checked the "It's an Expense" switch it will turn the entered amount negative
         if (lastEntry.isExpense) {
             entry *= -1
         }
 
+        //Now calculate and set new fortune and convert back to string
         fortune = (lastFortune + entry).toString()
     }
 }
--- dev/app/src/main/java/com/example/budgetduck/MainActivity.kt
+++ dev/app/src/main/java/com/example/budgetduck/MainActivity.kt
@@ -1,25 +1,33 @@
 package com.example.budgetduck
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TableLayout
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.green
+import androidx.core.graphics.red
+import androidx.core.view.marginStart
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
     private val entries = Entries()
-    private val allEntries = entries.getEntries()
     private lateinit var fortune: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Get Fortune field
         fortune = findViewById(R.id.fortune)
-
         fortune.setText(intent.getStringExtra("amount") ?: "0")
+
+        //This should've been the code to load all the entries, but unfortunately it didn't work
+        /*val test1: Entries = intent.getSerializableExtra("entries") as Entries ?: Entries()*/
 
         //Add entry Button
         val addEntry = findViewById<Button>(R.id.add_btn)
@@ -32,15 +40,75 @@ class MainActivity : AppCompatActivity() {
         //Reset Button
         val reset = findViewById<Button>(R.id.reset_btn)
         reset.setOnClickListener {
-            entries.entries.clear()
-            fortune.setText("0")
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder.setMessage("Are you sure you want to Reset?")
+                .setCancelable(false)
+                .setPositiveButton("Yes") { _, _ ->
+                    entries.entries.clear()
+                    fortune.setText("0")
+                }
+                .setNegativeButton("No") { dialog, _ ->
+                    dialog.dismiss()
+                }
+            val alert = builder.create()
+            alert.show()
         }
 
         //Load entries
+        loadEntries()
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun loadEntries() {
+        //Creating Mock List so something can be displayed
+        val testList = arrayListOf<Entry>()
+        testList.add(Entry("159.98", "Games", true))
+        testList.add(Entry("260", "Video Editing and Streaming", false))
+        testList.add(Entry("35.60", "Food", true))
+        testList.add(Entry("13.05", "Family and Friends", false))
+        testList.add(Entry("1360", "Paycheck", false))
+        testList.add(Entry("236.27", "Online Shopping", true))
+        testList.add(Entry("46", "Music", true))
+
+        //Get my entry table object
         val entryTable = findViewById<TableLayout>(R.id.entry_table)
 
-        for (entry in allEntries) {
+        //Here I would've used entries but I couldn't get it to work
 
+        if (testList.size > 0) {
+            //Go through all objects of array and create a table row and the texts for every entry
+            for (entry in testList) {
+                //Initialize new View objects
+                val newRow = TableRow(this)
+                val newCategory = TextView(this)
+                val newAmount = TextView(this)
+
+                //Set Text and color, if it's an expanse it'll add a - to the amount to make it negative
+                //Expenses will be marked red and revenues will be green
+                newCategory.text = entry.category
+                if (entry.isExpense){
+                    newAmount.text = "-${entry.amount}"
+                    newCategory.setTextColor(Color.parseColor("#FF0000"))
+                    newAmount.setTextColor(Color.parseColor("#FF0000"))
+                } else {
+                    newAmount.text = entry.amount
+                    newCategory.setTextColor(Color.parseColor("#00FF00"))
+                    newAmount.setTextColor(Color.parseColor("#00FF00"))
+                }
+
+                //Category style
+                newCategory.width = 275
+                newCategory.textSize = 20F
+
+                //Amount style
+                newAmount.textSize = 20F
+                newAmount.textAlignment = View.TEXT_ALIGNMENT_TEXT_END
+
+                //Add row and texts to Activity
+                entryTable.addView(newRow)
+                newRow.addView(newCategory)
+                newRow.addView(newAmount)
+            }
         }
     }
 }
